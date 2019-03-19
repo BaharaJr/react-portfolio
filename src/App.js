eaffd7b7a58887a9ac962fc22734fff4cb82eee1
Bennett <BaharaJr>
2019-03-19 23:19:47 +0300

    Added props and background image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './features/Navigation';
+import Header from './features/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
       <section>
-        <Navigation />
-      </section>
+        <Navigation />;
+        <Header aboutme = 'Bennett Benedict Bahara'/>
+      </section> 
     </div>
     );
   }
