189aaf8d53862f458375052770e7a6f756dae5a9
Bennett <BaharaJr>
2019-03-21 16:36:26 +0300

    Additions to my portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './features/Navigation';
-import Header from './features/Header';
+import { Layout, Header, Navigation, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <section>
-        <Navigation />;
-        <Header aboutme = 'Bennett Benedict Bahara'/>
-      </section> 
-    </div>
+      <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+
     );
   }
 }
